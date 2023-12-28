ffaed5e70e40521ff2ed789d78fce57a78d14f57
RonyAtoun <ronya@biot-med.com>
2023-12-28 09:56:37 +0200

    refactor: separated setup and teardown when two patients are needed as test data

--- test_abac_rules.py
+++ test_abac_rules.py
@@ -1,18 +1,20 @@
 import uuid
 import os
+
 from helpers import (login_with_with_credentials, create_registration_code, delete_registration_code, create_device,
                      identified_self_signup_with_registration_code, anonymous_self_signup_with_registration_code,
                      delete_patient, get_device, delete_device, create_organization, delete_organization,
                      update_organization, get_organization, get_organization_list, create_patient, update_patient,
                      get_patient, get_patient_list, change_patient_state)
 
-user_name = os.getenv('USERNAME')  # Has to be set in the environment in advance
-pass_word = os.getenv('PASSWORD')
 
-admin_auth_token = login_with_with_credentials(user_name, pass_word)
+#############################################################################################
+# Username and password have to be set in the environment in advance for each individual test
+#############################################################################################
 
 
 def test_patient_organization_abac_rules():
+    admin_auth_token = login_with_with_credentials(os.getenv('USERNAME'), os.getenv('PASSWORD'))
     # create organization
     template_id = "2aaa71cf-8a10-4253-9576-6fd160a85b2d"
     create_organization_response = create_organization(admin_auth_token, template_id)
@@ -75,35 +77,12 @@ def test_patient_organization_abac_rules():
 
 
 def test_patient_patient_abac_rules():
+    admin_auth_token = login_with_with_credentials(os.getenv('USERNAME'), os.getenv('PASSWORD'))
     # create two patients, registration codes and devices
-    patient_id = []
-    email = []
-    device_id = []
-    registration_code = []
-    registration_code_id = []
-    for n in range(2):
-        test_name = {"firstName": f'first_name_test_{uuid.uuid4().hex}'[0:35],
-                     "lastName": f'last_name_test_{uuid.uuid4().hex}'[0:35]}
-        tmp = (f'integ_test_{uuid.uuid4().hex}'[0:16])
-        email.append(tmp + '_@biotmail.com')
-        template_name = "RegistrationCode"
-        create_registration_code_response = create_registration_code(admin_auth_token, template_name, str(uuid.uuid4()))
-        assert create_registration_code_response.status_code == 201
-        registration_code.append(create_registration_code_response.json()['_code'])
-        registration_code_id.append(create_registration_code_response.json()['_id'])
-        device_template_name = 'DeviceType1'
-        device_id.append(f'test_{uuid.uuid4().hex}'[0:16])
-        create_device_response = create_device(admin_auth_token, device_template_name, device_id[n],
-                                               registration_code_id[n])
-        assert create_device_response.status_code == 201
-        self_signup_response = identified_self_signup_with_registration_code(admin_auth_token, test_name, email[n],
-                                                                             registration_code[n],
-                                                                             "00000000-0000-0000-0000-000000000000")
-        assert self_signup_response.status_code == 201
-        patient_id.append(self_signup_response.json()['patient']['_id'])
+    patient_setup = self_signup_patient_setup(admin_auth_token)
+    patient_id = patient_setup['patient_id']
+    patient_auth_token = patient_setup['patient_auth_token']
 
-    # login with one of the two patients
-    patient_auth_token = login_with_with_credentials(email[0], "Ab12z456")
     # Create and delete patient should fail
     test_name = {"firstName": f'first_name_test_{uuid.uuid4().hex}'[0:35],
                  "lastName": f'last_name_test_{uuid.uuid4().hex}'[0:35]}
@@ -146,6 +125,71 @@ def test_patient_patient_abac_rules():
     change_patient_state_response = change_patient_state(patient_auth_token, patient_id[0], "ENABLED")
     assert change_patient_state_response.status_code == 401
 
+    # Teardown
+    self_signup_patient_teardown(admin_auth_token, patient_setup)
+
+
+def test_patient_caregiver_abac_rules():
+    admin_auth_token = login_with_with_credentials(os.getenv('USERNAME'), os.getenv('PASSWORD'))
+    # create two patients, registration codes and devices
+    patient_setup = self_signup_patient_setup(admin_auth_token)
+    patient_id = patient_setup['patient_id']
+    email = patient_setup['email']
+    device_id = patient_setup['device_id']
+    registration_code = patient_setup['registration_code']
+    registration_code_id = patient_setup['registration_code_id']
+    patient_auth_token = patient_setup['patient_auth_token']
+
+    # Teardown
+    self_signup_patient_teardown(admin_auth_token, patient_setup)
+
+
+def self_signup_patient_setup(admin_auth_token):
+    # create two patients, registration codes and devices
+    patient_id = []
+    email = []
+    device_id = []
+    registration_code = []
+    registration_code_id = []
+    for n in range(2):
+        test_name = {"firstName": f'first_name_test_{uuid.uuid4().hex}'[0:35],
+                     "lastName": f'last_name_test_{uuid.uuid4().hex}'[0:35]}
+        tmp = (f'integ_test_{uuid.uuid4().hex}'[0:16])
+        email.append(tmp + '_@biotmail.com')
+        template_name = "RegistrationCode"
+        create_registration_code_response = create_registration_code(admin_auth_token, template_name, str(uuid.uuid4()))
+        assert create_registration_code_response.status_code == 201
+        registration_code.append(create_registration_code_response.json()['_code'])
+        registration_code_id.append(create_registration_code_response.json()['_id'])
+        device_template_name = 'DeviceType1'
+        device_id.append(f'test_{uuid.uuid4().hex}'[0:16])
+        create_device_response = create_device(admin_auth_token, device_template_name, device_id[n],
+                                               registration_code_id[n])
+        assert create_device_response.status_code == 201
+        self_signup_response = identified_self_signup_with_registration_code(admin_auth_token, test_name, email[n],
+                                                                             registration_code[n],
+                                                                             "00000000-0000-0000-0000-000000000000")
+        assert self_signup_response.status_code == 201
+        patient_id.append(self_signup_response.json()['patient']['_id'])
+
+    # login with one of the two patients
+    patient_auth_token = login_with_with_credentials(email[0], "Ab12z456")
+    return {
+        'patient_id': patient_id,
+        'email': email,
+        'device_id': device_id,
+        'registration_code': registration_code,
+        'registration_code_id': registration_code_id,
+        'patient_auth_token': patient_auth_token
+    }
+
+
+def self_signup_patient_teardown(admin_auth_token, patient_setup):
+    patient_id = patient_setup['patient_id']
+    email = patient_setup['email']
+    device_id = patient_setup['device_id']
+    registration_code = patient_setup['registration_code']
+    registration_code_id = patient_setup['registration_code_id']
     # Teardown
     for n in range(2):
         delete_patient_response = delete_patient(admin_auth_token, patient_id[n])
