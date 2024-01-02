d60e73df289e326fbcd38bd61f6e8afe5d383635
RonyAtoun <ronya@biot-med.com>
2024-01-02 12:15:26 +0200

    separated create template to generic entity and device

--- test_patient_abac_rules.py
+++ test_patient_abac_rules.py
@@ -9,7 +9,7 @@ from helpers import (login_with_with_credentials,
                      create_patient, update_patient, get_patient, get_patient_list, change_patient_state,
                      delete_patient,
                      create_device, get_device, delete_device, update_device, get_device_list,
-                     create_template, delete_template,
+                     create_organization_template, create_device_template, delete_template,
                      create_organization, delete_organization,
                      create_organization_user, delete_organization_user, update_organization_user,
                      change_organization_user_state, get_organization_user, get_organization_user_list,
@@ -24,7 +24,7 @@ from helpers import (login_with_with_credentials,
 # Username and password have to be set in the environment in advance for each individual test
 #############################################################################################
 
-
+# @pytest.mark.skip
 def test_patient_organization_abac_rules():
     admin_auth_token = login_with_with_credentials(os.getenv('USERNAME'), os.getenv('PASSWORD'))
     # create organization
@@ -32,9 +32,12 @@ def test_patient_organization_abac_rules():
     create_organization_response = create_organization(admin_auth_token, template_id)
     assert create_organization_response.status_code == 201
     organization_id = create_organization_response.json()['_id']
+    # create device template in new organization   ######!!!!!!!!!!!!!!!!!
+    device_template = create_template_setup(admin_auth_token, organization_id, "device")
+    device_template_name = device_template[2]
 
     # create patient, registration and device
-    patient_setup = self_signup_patient_setup(admin_auth_token, organization_id)
+    patient_setup = self_signup_patient_setup(admin_auth_token, organization_id, device_template_name)
     patient_auth_token = patient_setup['patient_auth_token']
 
     # create, update  and delete organization must fail
@@ -81,7 +84,8 @@ def test_patient_organization_users_abac_rules():
     organization_user_id = create_user_response.json()['_id']
 
     # create patient, registration and device
-    patient_setup = self_signup_patient_setup(admin_auth_token, "00000000-0000-0000-0000-000000000000")
+    patient_setup = self_signup_patient_setup(admin_auth_token, "00000000-0000-0000-0000-000000000000",
+                                              'DeviceType1')
     patient_auth_token = patient_setup['patient_auth_token']
 
     # create organization user by patient should fail
@@ -130,7 +134,8 @@ def test_patient_organization_users_abac_rules():
 def test_patient_patient_abac_rules():
     admin_auth_token = login_with_with_credentials(os.getenv('USERNAME'), os.getenv('PASSWORD'))
     # create two patients, registration codes and devices
-    patient_setup = self_signup_patient_setup(admin_auth_token, "00000000-0000-0000-0000-000000000000")
+    patient_setup = self_signup_patient_setup(admin_auth_token, "00000000-0000-0000-0000-000000000000",
+                                              'DeviceType1')
     patient_id = patient_setup['patient_id']
     patient_auth_token = patient_setup['patient_auth_token']
 
@@ -187,7 +192,8 @@ def test_patient_patient_abac_rules():
 def test_patient_caregiver_abac_rules():
     admin_auth_token = login_with_with_credentials(os.getenv('USERNAME'), os.getenv('PASSWORD'))
     # create two patients, registration codes and devices
-    patient_setup = self_signup_patient_setup(admin_auth_token, "00000000-0000-0000-0000-000000000000")
+    patient_setup = self_signup_patient_setup(admin_auth_token, "00000000-0000-0000-0000-000000000000",
+                                              'DeviceType1')
     patient_id = patient_setup['patient_id']
     patient_auth_token = patient_setup['patient_auth_token']  # logged in with first of two emails created
     patient_email = patient_setup['email']
@@ -260,7 +266,8 @@ def test_patient_caregiver_abac_rules():
 def test_patient_devices_abac_rules():
     admin_auth_token = login_with_with_credentials(os.getenv('USERNAME'), os.getenv('PASSWORD'))
     # create two patients, registration codes and devices
-    patient_setup = self_signup_patient_setup(admin_auth_token, "00000000-0000-0000-0000-000000000000")
+    patient_setup = self_signup_patient_setup(admin_auth_token, "00000000-0000-0000-0000-000000000000",
+                                              'DeviceType1')
     device_id = patient_setup['device_id']
     registration_code_id = patient_setup['registration_code_id']
     patient_auth_token = patient_setup['patient_auth_token']
@@ -303,7 +310,8 @@ def test_patient_devices_abac_rules():
 def test_patient_generic_entity_abac_rules():
     admin_auth_token = login_with_with_credentials(os.getenv('USERNAME'), os.getenv('PASSWORD'))
     # create two patients, registration codes and devices
-    patient_setup = self_signup_patient_setup(admin_auth_token, "00000000-0000-0000-0000-000000000000")
+    patient_setup = self_signup_patient_setup(admin_auth_token, "00000000-0000-0000-0000-000000000000",
+                                              'DeviceType1')
     patient_auth_token = patient_setup['patient_auth_token']
     patient_email = patient_setup['email']
     # create organization
@@ -312,7 +320,8 @@ def test_patient_generic_entity_abac_rules():
     assert create_organization_response.status_code == 201
     organization_id = create_organization_response.json()['_id']
     # create generic entity template
-    generic_entity_template_id = create_template_setup(admin_auth_token)
+    generic_entity_template_id = create_template_setup(admin_auth_token, "00000000-0000-0000-0000-000000000000",
+                                                       "organization")
 
     # create generic entity by patient only for self organization
     create_generic_entity_response = create_generic_entity(patient_auth_token, generic_entity_template_id,
@@ -383,8 +392,9 @@ def test_patient_generic_entity_abac_rules():
 def test_patient_registration_codes_abac_rules():
     admin_auth_token = login_with_with_credentials(os.getenv('USERNAME'), os.getenv('PASSWORD'))
     #### Setup  ####
-    # create patients, registration code and device in default organization
-    patient_setup = self_signup_patient_setup(admin_auth_token, "00000000-0000-0000-0000-000000000000")
+    # create patients, registration codes and devices in default organization
+    patient_setup = self_signup_patient_setup(admin_auth_token, "00000000-0000-0000-0000-000000000000",
+                                              'DeviceType1')
     patient_auth_token = patient_setup['patient_auth_token']
     patient_email = patient_setup['email']
     registration_code = patient_setup['registration_code']
@@ -397,6 +407,11 @@ def test_patient_registration_codes_abac_rules():
                                                                   "00000000-0000-0000-0000-000000000000")
     assert create_registration_code1_response.status_code == 201
     registration_code1_id = create_registration_code1_response.json()['_id']
+
+    # update registration code should succeed in same organization (code1 - using same code)
+    update_registration_code_response = update_registration_code(patient_auth_token, registration_code1_id,
+                                                                 str(uuid.uuid4()))
+    assert update_registration_code_response.status_code == 200
 
     # create registration code in different organization should fail
     # first create new organization
@@ -411,20 +426,8 @@ def test_patient_registration_codes_abac_rules():
                                                                   registration_code2, organization_id)
     assert create_registration_code2_response.status_code == 403
 
-    # for update test create registration code 2 by admin in new organization
-    registration_code2 = str(uuid.uuid4())
-    create_registration_code2_response = create_registration_code(admin_auth_token, "RegistrationCode",
-                                                                  registration_code2, organization_id)
-    assert create_organization_response.status_code == 201
-    registration_code2_id = create_registration_code2_response.json()['_id']
-
-    # update registration code should succeed in same organization (code1)
+    # update registration code (code1) by patient of default organization in other organization should fail
     update_registration_code_response = update_registration_code(patient_auth_token, registration_code1_id,
-                                                                 str(uuid.uuid4()))
-    assert update_registration_code_response.status_code == 200
-
-    # update registration code (code2) by patient of default organization in other organization should fail
-    update_registration_code_response = update_registration_code(patient_auth_token, registration_code2_id,
                                                                  str(uuid.uuid4()))
     assert update_registration_code_response.status_code == 403
 
@@ -464,7 +467,7 @@ def test_patient_locales_abac_rules():
     pass
 
 
-def self_signup_patient_setup(admin_auth_token, organization_id):
+def self_signup_patient_setup(admin_auth_token, organization_id, device_template_name):
     # create two patients, registration codes and devices
     patient_id = []
     email = []
@@ -476,11 +479,12 @@ def self_signup_patient_setup(admin_auth_token, organization_id):
                      "lastName": f'last_name_test_{uuid.uuid4().hex}'[0:35]}
         email.append(f'integ_test_{uuid.uuid4().hex}'[0:16] + '_@biotmail.com')
         template_name = "RegistrationCode"
-        create_registration_code_response = create_registration_code(admin_auth_token, template_name, str(uuid.uuid4()))
+        create_registration_code_response = create_registration_code(admin_auth_token, template_name,
+                                                                     str(uuid.uuid4()), organization_id)
         assert create_registration_code_response.status_code == 201
         registration_code.append(create_registration_code_response.json()['_code'])
         registration_code_id.append(create_registration_code_response.json()['_id'])
-        device_template_name = 'DeviceType1'
+        # device_template_name = 'DeviceType1'
         device_id.append(f'test_{uuid.uuid4().hex}'[0:16])
         create_device_response = create_device(admin_auth_token, device_template_name, device_id[n],
                                                registration_code_id[n])
@@ -516,13 +520,22 @@ def self_signup_patient_teardown(admin_auth_token, patient_setup):
         assert delete_registration_response.status_code == 204
 
 
-def create_template_setup(auth_token):
+def create_template_setup(auth_token, organization_id, entity_type):
     test_display_name = f'test_templ_{uuid.uuid4().hex}'[0:35]
     test_name = f'rony_test_{uuid.uuid4().hex}'[0:35]
     test_referenced_attrib_name = f'my built in attribute_{uuid.uuid4().hex}'[0:35]
     test_reference_attrib_display_name = test_referenced_attrib_name
-    create_template_response = create_template(auth_token, test_display_name, test_name, test_referenced_attrib_name,
-                                               test_reference_attrib_display_name)
+    if entity_type is "organization":
+        create_template_response = create_organization_template(auth_token, test_display_name, test_name,
+                                                                test_referenced_attrib_name,
+                                                                test_reference_attrib_display_name, organization_id)
+    elif entity_type is "device":
+        create_device_template(auth_token, test_display_name, test_name,
+                               test_referenced_attrib_name,
+                               test_reference_attrib_display_name, organization_id)
+    else:
+        pass
 
     assert create_template_response.status_code == 201
-    return create_template_response.json()['id']
+    return (create_template_response.json()['id'], create_template_response.json()['name'],
+            create_template_response.json()['displayName'])
