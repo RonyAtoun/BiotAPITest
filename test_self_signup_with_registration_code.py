661250a83edb4191ff8bf2902593e8cd89ea1a59
RonyAtoun <ronya@biot-med.com>
2024-01-04 08:20:24 +0200

    updated with changes in APIs in helpers.py

--- test_self_signup_with_registration_code.py
+++ test_self_signup_with_registration_code.py
@@ -6,19 +6,20 @@ from helpers import (login_with_with_credentials, create_registration_code, dele
 
 
 def test_identified_self_signup():
+    organization_id = "00000000-0000-0000-0000-000000000000"
     auth_token = login_with_with_credentials(os.getenv('USERNAME'), os.getenv('PASSWORD'))
     test_name = {"firstName": f'first_name_test_{uuid.uuid4().hex}'[0:35],
                  "lastName": f'last_name_test_{uuid.uuid4().hex}'[0:35]}
     email = f'integ_test_{uuid.uuid4().hex}'[0:16]
     email = email + '_@biotmail.com'
     template_name = "RegistrationCode"
-    create_registration_code_response = create_registration_code(auth_token, template_name, str(uuid.uuid4()))
+    create_registration_code_response = create_registration_code(auth_token, template_name, str(uuid.uuid4()), organization_id)
     assert create_registration_code_response.status_code == 201
     registration_code = create_registration_code_response.json()['_code']
     registration_code_id = create_registration_code_response.json()['_id']
     device_template_name = 'DeviceType1'
     device_id = f'test_{uuid.uuid4().hex}'[0:16]
-    create_device_response_code = create_device(auth_token, device_template_name, device_id, registration_code_id)
+    create_device_response_code = create_device(auth_token, device_template_name, device_id, registration_code_id, organization_id)
     assert create_device_response_code.status_code == 201
     self_signup_response_code = identified_self_signup_with_registration_code(auth_token, test_name, email,
                                                                               registration_code,
@@ -40,18 +41,19 @@ def test_identified_self_signup():
 
 
 def test_anonymous_self_signup():
+    organization_id = "00000000-0000-0000-0000-000000000000"
     auth_token = login_with_with_credentials(os.getenv('USERNAME'), os.getenv('PASSWORD'))
     nick_name = f'nickname_test_{uuid.uuid4().hex}'[0:35]
     email = f'integ_test_{uuid.uuid4().hex}'[0:16]
     email = email + '_@biotmail.com'
     template_name = "RegistrationCode"
-    create_registration_code_response = create_registration_code(auth_token, template_name, str(uuid.uuid4()))
+    create_registration_code_response = create_registration_code(auth_token, template_name, str(uuid.uuid4()), organization_id)
     assert create_registration_code_response.status_code == 201
     registration_code = create_registration_code_response.json()['_code']
     registration_code_id = create_registration_code_response.json()['_id']
     device_template_name = 'DeviceType1'
     device_id = f'test_{uuid.uuid4().hex}'[0:16]
-    create_device_response_code = create_device(auth_token, device_template_name, device_id, registration_code_id)
+    create_device_response_code = create_device(auth_token, device_template_name, device_id, registration_code_id, organization_id)
     assert create_device_response_code.status_code == 201
     self_signup_response_code = anonymous_self_signup_with_registration_code(auth_token, nick_name, email,
                                                                              registration_code)
