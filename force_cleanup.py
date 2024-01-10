02f412618be5475180480d11fba66c249b56880c
RonyAtoun <ronya@biot-med.com>
2024-01-10 17:53:20 +0200

    Deleted devices and patients

--- force_cleanup.py
+++ force_cleanup.py
@@ -35,9 +35,24 @@ def cleanup_generic_entities():
             get_generic_entity_list_response = get_generic_entity_list(admin_auth_token)
             assert get_generic_entity_list_response.status_code == 200
             for generic_entity in get_generic_entity_list_response.json()['data']:
-                print(generic_entity['_name'])
+                print('generic entity', generic_entity['_name'])
                 delete_generic_entity_response = delete_generic_entity(admin_auth_token, generic_entity['_id'])
                 assert delete_generic_entity_response.status_code == 204
+
+    get_device_list_response = get_device_list(admin_auth_token)
+    assert get_device_list_response.status_code == 200
+    for device in get_device_list_response.json()['data']:
+        print("deviceId", device['_id'])
+        if 'test' in device['_id']:
+            delete_device_response = delete_device(admin_auth_token, device['_id'])
+            assert delete_device_response.status_code == 204
+    get_patient_list_response = get_patient_list(admin_auth_token)
+    assert get_patient_list_response.status_code == 200
+    for patient in get_patient_list_response.json()['data']:
+        if "test" in patient['_name']['firstName']:
+            print("patient", patient['_name'])
+            patient_delete_response = delete_patient(admin_auth_token, patient['_id'])
+            assert patient_delete_response.status_code == 204
 
 
 if __name__ == "__main__":
