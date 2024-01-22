c10b97b818d14548fee786269f54b017112877ee
RonyAtoun <ronya@biot-med.com>
2024-01-22 14:37:28 +0200

    Deleted devices and patients

--- force_cleanup.py
+++ force_cleanup.py
@@ -27,18 +27,6 @@ from API_drivers import (
 
 def cleanup_generic_entities():
     admin_auth_token = login_with_with_credentials(os.getenv('USERNAME'), os.getenv('PASSWORD'))
-    get_organization_list_response = get_organization_list(admin_auth_token)
-    assert get_organization_list_response.status_code == 200
-    for org in get_organization_list_response.json()['data']:
-        if "Test Org_" in org['_name']:
-            print(org['_name'], org['_id'])
-            get_generic_entity_list_response = get_generic_entity_list(admin_auth_token)
-            assert get_generic_entity_list_response.status_code == 200
-            for generic_entity in get_generic_entity_list_response.json()['data']:
-                print('generic entity', generic_entity['_name'])
-                delete_generic_entity_response = delete_generic_entity(admin_auth_token, generic_entity['_id'])
-                assert delete_generic_entity_response.status_code == 204
-
     get_device_list_response = get_device_list(admin_auth_token)
     assert get_device_list_response.status_code == 200
     for device in get_device_list_response.json()['data']:
@@ -53,6 +41,20 @@ def cleanup_generic_entities():
             print("patient", patient['_name'])
             patient_delete_response = delete_patient(admin_auth_token, patient['_id'])
             assert patient_delete_response.status_code == 204
+
+    get_organization_list_response = get_organization_list(admin_auth_token)
+    assert get_organization_list_response.status_code == 200
+    for org in get_organization_list_response.json()['data']:
+        if "Test Org_" in org['_name']:
+            print(org['_name'], org['_id'])
+            get_generic_entity_list_response = get_generic_entity_list(admin_auth_token)
+            assert get_generic_entity_list_response.status_code == 200
+            for generic_entity in get_generic_entity_list_response.json()['data']:
+                print('generic entity', generic_entity['_name'])
+                delete_generic_entity_response = delete_generic_entity(admin_auth_token, generic_entity['_id'])
+                assert delete_generic_entity_response.status_code == 204
+            delete_organization_response = delete_organization(admin_auth_token, org['_id'])
+            assert delete_organization_response.status_code == 204
 
 
 if __name__ == "__main__":
