efa38154c99872492209c0d3a84e8d1d7c37d640
RonyAtoun <ronya@biot-med.com>
2024-02-18 16:22:54 +0200

    refactored by regina

--- accept_invitation.py
+++ accept_invitation.py
@@ -1,20 +1,17 @@
 import os
-
-import pytest
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import unquote, parse_qs, urlparse
+import time
 
-MAX_RETRIES = 3
+MAX_RETRIES = 7
 
-slack_token = os.getenv('SLACK')
+slack_token = os.getenv('SLACK_TOKEN')
 channel_id = "C04M7N12T7G"
 url = f"https://slack.com/api/conversations.history"
 
 
-@pytest.fixture()
-def get_user_token_and_id():
-    user_email = "oksana+31@biotmail.com"
+def accept_invitation(user_email):
     headers = {
         "Authorization": f"Bearer {slack_token}",
         "Content-Type": "application/json"
@@ -29,6 +26,7 @@ def get_user_token_and_id():
             messages = data.get("messages", [])[0:5]
             for message in messages:
                 user_email_in_letter = (((message.get("files")[0])["to"])[0])["address"]
+                time.sleep(5)
                 if user_email_in_letter == user_email:
                     html_content = (message.get("files")[0])["preview"]
                     soup = BeautifulSoup(html_content, 'html.parser')
@@ -38,20 +36,14 @@ def get_user_token_and_id():
                     query_params = parse_qs(parsed_url.query)
                     token = query_params.get('token', [''])[0]
                     entity_id = query_params.get('entityId', [''])[0]
-                    print(f"This is token {token} for entity id {entity_id}")
-                    return token, entity_id, user_email
+                    signup_url = f"https://api.qa.biot-gen2.biot-med.com/organization/v1/tokens/{token}?entityId={entity_id}" \
+                                 f"&operation=accept-invitation"
+                    headers = {'Content-Type': 'application/json'}
+                    payload = f"{{\"operationData\":{{\"password\":\"Aa123456strong\",\"username\":\"{user_email}\"}}}}"
+                    response = requests.request("POST", signup_url, headers=headers, data=payload)
+                    assert response.status_code == 200, f"Error: {response.status_code}, {response.text}"
+                    return response.text
         else:
-            print(f"Error: {response.status_code}, {response.text}")
+            raise Exception(f"Error: {response.status_code}, {response.text}")
     else:
-        print("Number of attempts to retrieve email has been used. No email found.")
-
-
-def test_accept_invitation(get_user_token_and_id):
-    token, entity_id, user_email = get_user_token_and_id
-    signup_url = f"https://api.qa.biot-gen2.biot-med.com/organization/v1/tokens/{token}?entityId={entity_id}" \
-                 f"&operation=accept-invitation"
-    headers = {'Content-Type': 'application/json'}
-    payload = f"{{\"operationData\":{{\"password\":\"Aa123456\",\"username\":\"{user_email}\"}}}}"
-    response = requests.request("POST", signup_url, headers=headers, data=payload)
-    assert response.status_code == 200, f"Error: {response.status_code}, {response.text}"
-    print(response.text)
+        raise Exception(f"Error: Number of {MAX_RETRIES} attempts to retrieve email has been used. No email found.")
